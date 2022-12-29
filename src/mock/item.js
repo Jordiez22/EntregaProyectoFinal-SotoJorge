7ffaaacd019828ea0a9e2753f1ef392169c31cae
Jorge Soto <junaa@gmail.com>
2022-12-29 12:21:56 -0500

    add new file:   .gitignore add new file:   README.md add new file:   package-lock.json add new file:   package.json add new file:   public/favicon.ico add new file:   public/img.html/Img7.jpg add new file:   public/img.html/img1.jpg add new file:   public/img.html/img10.jpg add new file:   public/img.html/img2.jpg add new file:   public/img.html/img3.jpg add new file:   public/img.html/img4.jpg add new file:   public/img.html/img5.png add new file:   public/img.html/img6.jpg add new file:   public/img.html/img8.jpg add new file:   public/img.html/img9.jpg add new file:   public/index.html add new file:   public/logo192.png add new file:   public/logo512.png add new file:   public/manifest.json add new file:   public/robots.txt add new file:   src/App.css add new file:   src/App.js add new file:   src/App.test.js add new file:   src/components/CartWidget/CartWidget.css add new file:   src/components/CartWidget/CartWidget.jsx add new file:   src/components/Item/Item.css add new file:   src/components/Item/Item.jsx add new file:   src/components/ItemCount/ItemCount.css add new file:   src/components/ItemCount/ItemCount.jsx add new file:   src/components/ItemDetail/ItemDetail.css ad new file:   src/components/ItemDetail/ItemDetail.jsx add new file:   src/components/ItemDetailContainer/ItemDetailContainer.css add new file:   src/components/ItemDetailContainer/ItemDetailContainer.jsx add new file:   src/components/ItemListContainer/ItemListContainer.css add new file:   src/components/ItemListContainer/ItemListContainer.jsx add new file:   src/components/Navbar/NavBar.jsx add new file:   src/components/Navbar/Navbar.css add new file:   src/containers/ItemDetail/ItemDetailContainer.js add new file:   src/containers/ItemDetail/ItemDetailContainer.scss add new file:   src/containers/ItemList/ItemListContainer.js add new file:   src/containers/ItemList/ItemListContainer.scss add new file:   src/containers/NavBar/NavBarContainer.js add new file:   src/containers/NavBar/NavBarContainer.scss add new file:   src/firebase/config.js add new file:   src/index.css add	new file:   src/index.js add	new file:   src/logo.svg add new file:   src/main.jsx add new file:   src/mock/item.js add new file:   src/reportWebVitals.js add new file:   src/setupTests.js add new file:   tailwind.config.js

---
+++ src/mock/item.js
@@ -0,0 +1,41 @@
+export const products = [
+
+{
+id: 1,
+stock: 10,
+title: 'Delineado', 
+price: 3000,
+category: 'Servicios',
+description: 'Delineado Dermopigmentación'
+},
+
+{ 
+id: 2
+stock: 8,
+title: 'Cejas',
+price: 3000,
+category: 'Servicios',
+description:'Microblading Láser Y Dermopigmentación'
+},
+
+{
+ide: 3
+stock: 10,
+title: 'Labios',
+price: 3000, 
+category: 'Servicios', 
+description:'Labios Micropigmentación',
+}
+
+{
+id: 4
+stock: 9,
+title: 'Remocion', 
+price: 3500,
+category: 'Servicios',
+ description:'Remoción'
+
+}
+];
+
+
